7717446d1f868665f049d3c8982eb645a39dcacd
Milos Vasic <i@mvasic.ru>
2025-10-01 19:44:10 +0300

    Cleaning up.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,6 @@ plugins {
     id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21" apply false
     id("com.github.gmazzo.buildconfig") version "5.6.7" apply false
     id("com.github.ben-manes.versions") version "0.52.0"
-
-    // id("androidx.baselineprofile") version "1.2.3" apply false
 }
 
 tasks.withType<DependencyUpdatesTask> {
